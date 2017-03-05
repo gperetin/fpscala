4880218a52a95149d4c63f176fae5fd6f1469e96
Goran Peretin <goran.peretin@gmail.com>
2017-03-05 10:57:09 -0800

    Add Option.scala

---
+++ Option.scala
@@ -0,0 +1,25 @@
+import scala.{Option => _, Either => _, _}
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+}
+
+case class Some[+A](get: A) extends Option[A]
+object None extends Option[Nothing]
